d4055516e1adabd18d26014ecd251cdcb6e3a9bb
Niklas Stich <32730297+niklasstich@users.noreply.github.com>
2020-10-18 03:49:42 +0200

    more work on web

--- js/animation.js
+++ js/animation.js
@@ -1,10 +1,22 @@
+const areaWidth = 240;
+const sliderWidth = 30;
+const middleLineX = 59;
 
+function main(){
+    let slider = document.querySelector("#slider");
+    slider.y = 0;
+    slider.x = 
+    anime({
+        targets: 'div', 
+        translateX: 250,
+        //rotate: '1turn',
+        //backgroundColor: '#FFF',
+        duration: 1500,
+        easing: 'easeInOutExpo',
+        direction: 'alternate',
+        loop: true,
+        autoplay: true
+    });
+}
 
-anime({
-    targets: 'div', 
-    translateX: 250,
-    rotate: '1turn',
-    backgroundColor: '#FFF',
-    duration: 800,
-    loop: true
-})
+document.addEventListener("DOMContentLoaded", main);
