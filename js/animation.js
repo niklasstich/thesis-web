c1ef808d7a4f8070fb306adf667df5c260679cf1
Niklas Stich <32730297+niklasstich@users.noreply.github.com>
2020-10-22 02:36:43 +0200

    Added animation to showcase svg animations

--- js/animation.js
+++ js/animation.js
@@ -1,22 +1,37 @@
-const areaWidth = 240;
-const sliderWidth = 30;
+const areaWidth = 140;
+const sliderWidth = 30-1;
 const middleLineX = 59;
+const sliderHeight = 240-1;
 
 function main(){
     let slider = document.querySelector("#slider");
-    slider.y = 0;
-    slider.x = 
+    slider.setAttribute('y', 0);
+    slider.setAttribute('x', middleLineX - sliderWidth/2);
+    slider.setAttribute('width', sliderWidth);
+    slider.setAttribute('height', sliderHeight);
     anime({
         targets: 'div', 
         translateX: 250,
         //rotate: '1turn',
         //backgroundColor: '#FFF',
         duration: 1500,
+        scale: 0.75,
         easing: 'easeInOutExpo',
         direction: 'alternate',
         loop: true,
         autoplay: true
     });
+
+    anime({
+        targets: '#slider',
+        y: "150",
+        height: "20",
+        easing: "easeOutExpo",
+        direction: "alternate",
+        delay: 500,
+        endDelay: 1000,
+        loop: true
+    })
 }
 
 document.addEventListener("DOMContentLoaded", main);
