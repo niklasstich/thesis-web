5ed5f93cad55db1772b965313eb7523b03ab6ba1
Niklas Stich <32730297+niklasstich@users.noreply.github.com>
2020-12-07 15:20:47 +0100

    Add all text fields required and put into <g> groups with enumeration

--- js/example.js
+++ js/example.js
@@ -1,13 +1,27 @@
+let diagramIteration = 0;
+
 function addDiagram() {
+    diagramIteration++;
     // get svg
     const svgObject = document.getElementById("svg");
+    const svgDoc = svgObject.ownerDocument;
     // make room for new diagram
     const viewBox = svgObject.getAttributeNS(null, "viewBox");
     const width = parseInt(viewBox.split(" ")[2])
     const height = parseInt(viewBox.split(" ")[3])
     svgObject.setAttributeNS(null, "viewBox", "0 0 " + (width+120) + " " + (height));
+    
+    // add groups
+    let group = svgDoc.createElementNS("http://www.w3.org/2000/svg", "g");
+    group.id = "diagram-"+diagramIteration;
+    let lineGroup = svgDoc.createElementNS("http://www.w3.org/2000/svg", "g");
+    let textGroup = svgDoc.createElementNS("http://www.w3.org/2000/svg", "g");
+    lineGroup.setAttribute("stroke", "black");
+    lineGroup.classList += "lines";
+    textGroup.classList += "text";
+
     // add shapes
-    const svgDoc = svgObject.ownerDocument;
+    // x1 x2 y1 y2
     const lineData = [
         [width, width+100, 1, 1],
         [width, width+100, 199, 199],
@@ -18,24 +32,32 @@ function addDiagram() {
     ];
     // draw lines
     lineData.forEach(element => {
-        newShape = svgDoc.createElementNS("http://www.w3.org/2000/svg", "line");
-        newShape.setAttributeNS(null, "x1", element[0]);
-        newShape.setAttributeNS(null, "x2", element[1]);
-        newShape.setAttributeNS(null, "y1", element[2]);
-        newShape.setAttributeNS(null, "y2", element[3]);
-        newShape.setAttributeNS(null, "stroke", "black");
-        svgObject.appendChild(newShape);
+        let newShape = svgDoc.createElementNS("http://www.w3.org/2000/svg", "line");
+        newShape.setAttribute("x1", element[0]);
+        newShape.setAttribute("x2", element[1]);
+        newShape.setAttribute("y1", element[2]);
+        newShape.setAttribute("y2", element[3]);
+        lineGroup.appendChild(newShape);
     })
     // draw letters
-    const letterData = [
-        [width+102, 5, "Max"],
-        [width+102, 199, "Min"]
+    // x y text
+    const textData = [
+        [width+102, 5, "Max", "max"],
+        [width+102, 199, "Min", "min"],
+        [width+67, 152, "Q1", "q1"],
+        [width+87, 102, "Q2", "q2"],
+        [width+67, 52, "Q3", "q3"]
     ]
-    letterData.forEach(element => {
-        newShape = svgDoc.createElementNS("http://www.w3.org/2000/svg", "text")
-        newShape.setAttributeNS(null, "x", element[0]);
-        newShape.setAttributeNS(null, "y", element[1]);
+    textData.forEach(element => {
+        let newShape = svgDoc.createElementNS("http://www.w3.org/2000/svg", "text")
+        newShape.setAttribute("x", element[0]);
+        newShape.setAttribute("y", element[1]);
         newShape.textContent = element[2];
-        svgObject.appendChild(newShape);
-    });
-    }
+        newShape.id = element[3];
+        textGroup.appendChild(newShape);
+    }); 
+    group.appendChild(lineGroup);
+    group.appendChild(textGroup);
+
+    svgObject.appendChild(group);
+}
